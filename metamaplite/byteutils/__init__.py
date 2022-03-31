189f3e4de6b24b2b06d7ec79f35531d5329db429
Willie Rogers <wjrogers@nih.gov>
2022-03-31 13:14:18 -0400

    Updated byteutils to support big as well as little endian byte order

--- metamaplite/byteutils/__init__.py
+++ metamaplite/byteutils/__init__.py
@@ -1,4 +1,10 @@
-""" bytes to integer and integer to byte conversion functions """
+"""bytes to integer and integer to byte conversion functions
+
+For python 3.2 and greater use int.from_bytes instead of bytes_to_int
+and int.to_bytes instead of int_to_bytes.  The python implementation
+is 4 times faster than this one.
+
+"""
 
 
 def bytes_to_int(bytes):
@@ -9,10 +15,11 @@ def bytes_to_int(bytes):
     return result
 
 
-def int_to_bytes(value, length):
+def int_to_bytes(value, length, byteorder='little'):
     "convert integer to bytes"
     result = []
     for i in range(0, length):
         result.append(value >> (i * 8) & 0xff)
-    result.reverse()
-    return result
+    if byteorder == 'big':
+        result.reverse()
+    return bytes(result)
